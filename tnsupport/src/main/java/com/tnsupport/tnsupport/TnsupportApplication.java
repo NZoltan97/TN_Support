963e2cda8b09aaf8d0aa1d565f2d3f29e8057421
Kornel Varga <kornel.varga97@gmail.com>
2018-07-17 13:01:23 +0200

    adds strarter project

---
+++ tnsupport/src/main/java/com/tnsupport/tnsupport/TnsupportApplication.java
@@ -0,0 +1,12 @@
+package com.tnsupport.tnsupport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TnsupportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TnsupportApplication.class, args);
+	}
+}
---
+++ tnsupport/src/test/java/com/tnsupport/tnsupport/TnsupportApplicationTests.java
@@ -0,0 +1,16 @@
+package com.tnsupport.tnsupport;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TnsupportApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
