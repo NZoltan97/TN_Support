ddbe548ab2eebd79f6896ca1a7d59c71d9d76755
kornelVarga <kornel.varga97@gmail.com>
2018-08-09 09:57:44 +0200

    calling endpoint

--- tnsupport/src/main/java/com/tnsupport/controllers/RequestController/IRequestController.java
+++ tnsupport/src/main/java/com/tnsupport/controllers/RequestController/IRequestController.java
@@ -1,5 +1,19 @@
 package com.tnsupport.controllers.RequestController;
 
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.tnsupport.dtos.AttributeDTO;
+import com.tnsupport.model.Performer;
+import com.tnsupport.model.SiteInfo;
+
 public interface IRequestController {
 
+	public ResponseEntity<AttributeDTO> sendSiteID(@RequestParam ("hostName") String hostName, @RequestParam ("siteIDAtt") String siteIDAtt);
+	
+	public SiteInfo sendSiteInfo();
+	
+	public List<Performer> sendPerformerInfo();
 }
--- tnsupport/src/main/java/com/tnsupport/services/RestTemplateService/IRestTemplateService.java
+++ tnsupport/src/main/java/com/tnsupport/services/RestTemplateService/IRestTemplateService.java
@@ -1,8 +1,14 @@
 package com.tnsupport.services.RestTemplateService;
 
+import java.util.List;
+
+import com.tnsupport.model.Performer;
 import com.tnsupport.model.SiteInfo;
 
 public interface IRestTemplateService {
 
 	public SiteInfo getSiteInfo();
+	
+	public List<Performer> getPerformers();
+	
 }
