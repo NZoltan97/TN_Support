522ba0b4e2fd6b55b0f001f77c525e1bd746892c
kornelVarga <kornel.varga97@gmail.com>
2018-08-16 13:18:30 +0200

    edits RestTemplateService

--- tnsupport/src/main/java/com/tnsupport/controllers/RequestController/IRequestController.java
+++ tnsupport/src/main/java/com/tnsupport/controllers/RequestController/IRequestController.java
@@ -8,16 +8,14 @@ import org.springframework.web.bind.annotation.RequestParam;
 import com.tnsupport.dtos.AttributeDTO;
 import com.tnsupport.dtos.ChatFuelDTO;
 import com.tnsupport.model.Location;
-import com.tnsupport.model.Performer;
 import com.tnsupport.model.Ticket;
-import com.tnsupport.model.Zone;
 
 public interface IRequestController {
 
 	public ResponseEntity<AttributeDTO> sendSiteId(@RequestParam ("siteId") String siteId);
 	public ChatFuelDTO getSiteInfo(@RequestParam ("siteId") String siteId);
-	public List<Performer> getPerformerInfo(@RequestParam ("siteId") String siteId);
-	public Zone[] getZoneInfo(@RequestParam ("siteId") String siteId);
+	public ChatFuelDTO getPerformerInfo(@RequestParam ("siteId") String siteId);
+	public ChatFuelDTO getZoneInfo(@RequestParam ("siteId") String siteId);
 	public List<Ticket> getTicketInfo(@RequestParam ("siteId") String siteId);
 	public List<Location> getLocationInfo(@RequestParam ("siteId") String siteId);
 }
--- tnsupport/src/main/java/com/tnsupport/services/RestTemplateService/IRestTemplateService.java
+++ tnsupport/src/main/java/com/tnsupport/services/RestTemplateService/IRestTemplateService.java
@@ -5,17 +5,15 @@ import java.util.List;
 import com.tnsupport.dtos.ChatFuelDTO;
 import com.tnsupport.dtos.InnerDTO;
 import com.tnsupport.model.Location;
-import com.tnsupport.model.Performer;
 import com.tnsupport.model.Ticket;
-import com.tnsupport.model.Zone;
 
 public interface IRestTemplateService {
 
 	public ChatFuelDTO getSiteInfo(InnerDTO innerDto);
 
-	public List<Performer> getPerformers(InnerDTO innerDto);
+	public ChatFuelDTO getPerformers(InnerDTO innerDto);
 
-	public Zone[] getZones(InnerDTO innerDto);
+	public ChatFuelDTO getZones(InnerDTO innerDto);
 
 	public List<Ticket> getTickets(InnerDTO innerDto);
 
