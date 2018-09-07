a72ee0b6f1794a8c1444cbcb6105f34220283b0b
kornelVarga <kornel.varga97@gmail.com>
2018-09-07 15:54:15 +0200

    modifies Button class

--- tnsupport/src/main/java/com/tnsupport/dtos/gallery/Button.java
+++ tnsupport/src/main/java/com/tnsupport/dtos/gallery/Button.java
@@ -6,12 +6,12 @@ import lombok.Data;
 public class Button {
 
 	private String type;
-	private String url;
+	private String block_names;
 	private String title;
 	
 	public Button(String type, String url, String title) {
 		this.type = type;
-		this.url = url;
+		this.block_names = url;
 		this.title = title;
 	}
 }
